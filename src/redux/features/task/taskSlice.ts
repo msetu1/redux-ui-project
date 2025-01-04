e8b528a31d41f3a85eb3655b7a404df7059ad206
msetu1 <msetu5763@gmail.com>
2025-01-04 21:35:31 +0600

    code add

--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -4,42 +4,47 @@ import { createSlice } from "@reduxjs/toolkit";
 
 interface IInitialState {
   tasks: ITask[];
-  filter:"all"|"High" | "Medium" | "Low"
+  filter: "all" | "High" | "Medium" | "Low";
 }
 
 const initialState: IInitialState = {
   tasks: [
-    {
-      id: "01",
-      title: "Initialized Frontend",
-      description:
-        "Seamlessly promote efficient collaboration and idea-sharing through client-centric collaboration and idea-sharing.",
-      dueDate: "2025-11",
-      isCompleted: false,
-      priority: "High",
-    },
-    {
-      id: "02",
-      title: "Back Frontend",
-      description:
-        "Seamlessly promote efficient collaboration and idea-sharing through client-centric collaboration and idea-sharing.",
-      dueDate: "2025-11",
-      isCompleted: false,
-      priority: "Low",
-    },
+    // {
+    //   id: "01",
+    //   title: "Initialized Frontend",
+    //   description:
+    //     "Seamlessly promote efficient collaboration and idea-sharing through client-centric collaboration and idea-sharing.",
+    //   dueDate: "2025-11",
+    //   isCompleted: false,
+    //   priority: "High",
+    // },
+    // {
+    //   id: "02",
+    //   title: "Back Frontend",
+    //   description:
+    //     "Seamlessly promote efficient collaboration and idea-sharing through client-centric collaboration and idea-sharing.",
+    //   dueDate: "2025-11",
+    //   isCompleted: false,
+    //   priority: "Low",
+    // },
   ],
-  filter:'all',
+  filter: "all",
 };
 
 const taskSlice = createSlice({
   name: "task",
   initialState,
-  reducers: {},
+  reducers: {
+    addTask: (state, action) => {
+      state.tasks.push(action.payload);
+    },
+  },
 });
-export const selectTasks=(state:RootState)=>{
-    return state.todo.tasks;
-}
-export const selectFIlter=(state:RootState)=>{
-    return state.todo.filter;
-}
+export const selectTasks = (state: RootState) => {
+  return state.todo.tasks;
+};
+export const selectFIlter = (state: RootState) => {
+  return state.todo.filter;
+};
+export const { addTask } = taskSlice.actions;
 export default taskSlice.reducer;
